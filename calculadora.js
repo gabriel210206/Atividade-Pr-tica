53bbdc433ddf580fd34b53c20aaa81a9adbfede0
gabriel210206 <gabrielsveloso2@gmail.com>
2024-07-22 20:57:53 -0300

    Feature: added subtraction

--- calculadora.js
+++ calculadora.js
@@ -1,2 +1,22 @@
+const soma = () => {
+    console.log(parseInt(args[0]) + parseInt(args[1]));
+};
+
+const sub = () => {
+    console.log(parseInt(args[0]) - parseInt(args[1]));  
+}
+
 const args = process.argv.slice(2);
-console.log(parseInt(args[0]) + parseInt(args[1]));
+
+switch (args[0]) {
+    case 'soma':
+        soma();
+    break;
+
+    case 'sub':
+        sub();
+    break;
+
+    default:
+        console.log('does not support', arg[0]);
+}
